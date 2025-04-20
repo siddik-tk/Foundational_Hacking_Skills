79e0e7f7ea2bccacd9c867695deec8d7525ab5dc
siddik-tk <siddik3tk@gmail.com>
2025-04-20 23:47:15 +0530

    adding day2 and day3 files

---
+++ day3/try.py
@@ -0,0 +1,19 @@
+
+# print(f.read())
+# print("the file content in line 1: ",f.readline())
+# print("the file content in line 2: ",f.readline())
+# print("the file content in line 3: ",f.readline())
+try:
+    f = open("//home//siddiktk//Music//song.txt//sung","w")
+    f.write("\n musics are matter but!")
+    f.close()
+
+    f = open("//home//siddiktk//Music//song.txt//sung")
+    count = 0
+    for i in f:
+        count = count + 1
+        print(f"the file content in line {count}: ",i)
+except FileNotFoundError:
+    print("file not found.")
+    print("try again!.")
+
