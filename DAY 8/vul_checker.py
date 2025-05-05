3786dcabf705c45062c02a74344526ab761af3cd
siddik-tk <siddik3tk@gmail.com>
2025-05-06 01:16:00 +0530

    project update 1.1

--- DAY 8/vul_checker.py
+++ DAY 8/vul_checker.py
@@ -16,6 +16,8 @@ response = input("""enter the type of scan you wan to perform ------------>
 """)
 print(" you have entered :  ",response)
 
+vendor_list = []
+
 if int(response) == 1:
     print("performing syn scan...")
     print("version: ",scanner.nmap_version())
@@ -35,9 +37,9 @@ elif int(response) == 2:
         print("no open udp ports found!")
 
 elif int(response) == 3:
-    print("performing UDP scan...")
+    print("performing comprehensive scan...")
     print("version: ", scanner.nmap_version())
-    print(scanner.scan(ip_addr, '1-65500', '-v -sU'))
+    print(scanner.scan(ip_addr, '1-65500', '-v -sS -sV -sC -A'))
     if 'tcp' in scanner[ip_addr]:
         print("open ports: ", scanner[ip_addr]['tcp'].keys())
     else:
@@ -52,8 +54,9 @@ elif int(response) == 4:
             ports = scanner[host][proto].keys()
             for port in ports:
                 service = scanner[host][proto][port]
-                vendor = []
-                vendor = service['product']
+                vendor = service.get('product','')
+                if vendor:
+                    vendor_list.append(vendor)
                 print(f"""port:   {port} 
 service: {service['name']}
 vendor:  {service['product']}
@@ -74,7 +77,7 @@ def vuln_checker(vendor):
     keyword = vendor
 
     params = {
-        'KeywordSearch': keyword,
+        'keywordSearch': keyword,
         'ResultsPerPage': 5
     }
 
@@ -85,11 +88,14 @@ def vuln_checker(vendor):
         for lst in result.get('vulnerabilities',[]):
             cve_info = lst.get('cve',[])
             cve_id = cve_info.get('id',[])
-            cve_desc = cve_info.get('descriptions')
+            cve_desc = cve_info.get('descriptions','')
 
 
             print(f"cve id : {cve_id}")
-            print(f"cve desccription : {cve_desc[0]}")
+            if cve_desc:
+                print(f"cve description : {cve_desc[0].get('value', 'No description available')}")
+            else:
+                print("No description available.")
 
         feed = int(input("give a feed back(out of 5 in numbers):"))
         if feed < 3:
@@ -102,7 +108,8 @@ def vuln_checker(vendor):
 vuln_check = input("do you want to check the vulnerability information from the scan(y/n):  ").lower().strip()
 
 if vuln_check == 'y':
-    vuln_checker(vendor)
+    for vndor in vendor_list:
+        vuln_checker(vndor)
 
 
 
