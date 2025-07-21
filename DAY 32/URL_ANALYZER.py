f0ff0e6d97aac978cccdedf92b1370e7306afb72
siddik-tk <siddik3tk@gmail.com>
2025-07-21 10:26:23 +0530

    DAY 32 FINISHED.URL_ANALYZER

---
+++ DAY 32/URL_ANALYZER.py
@@ -0,0 +1,26 @@
+import re
+from urllib.parse import urlparse
+
+shorteners = ['bit.ly', 'tinyurl.com', 't.co', 'goo.gl', 'ow.ly', 'is.gd']
+ip_regex = re.compile(r'\bhttp[s]?://\d{1,3}(?:\.\d{1,3}){3}\b')
+
+def is_suspicious(url):
+    parsed = urlparse(url)
+    netloc = parsed.netloc.lower()
+    if ip_regex.match(url):
+        return "IP-based URLs are suspicious [x]"
+    elif any(short in netloc for short in shorteners):
+        return "Shortened URL are very suspicious [x]"
+    elif "-" in netloc or netloc.count('.') >= 3:
+        return "Suspicious no of domains [!]"
+    else:
+        return "Looks Safe [-]"
+
+def scan_urls(file_path):
+    with open(file_path, 'r') as f:
+        urls = [line.strip() for line in f if line.strip()]
+    for url in urls:
+        result = is_suspicious(url)
+        print(f"[*] {url} → {result}")
+
+scan_urls("./urls.txt")
